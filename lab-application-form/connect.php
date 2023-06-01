e4768ecc5d5a80e0d4906a5d220eed7710df33fe
Enes YALÇIN <107759141+eenesyalcin@users.noreply.github.com>
2023-06-01 22:29:04 +0300

    Add files via upload

---
+++ lab-application-form/connect.php
@@ -0,0 +1,31 @@
+<!-- PHP CODES -->
+<?php
+
+    // Database connection codes.
+
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $dbn = "lab_application_form";
+
+
+    $connect = mysqli_connect($host, $user, $password, $dbn);
+    mysqli_set_charset($connect, "UTF8");
+
+    
+
+    // Database connection check
+
+    /*
+
+        if($connect){
+            echo "CONNECTION SUCCESSFUL";
+        }
+        else
+        {
+            echo "CONNECTION FAILED";
+        }
+
+    */
+    
+?> <!-- END OF PHP CODE BLOCK -->
